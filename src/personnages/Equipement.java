3ce2b9425795fe6a4e636733c713dbb443e7e930
PNL4817A <PNL4817A@u3-215-03.ens.fsi>
2023-03-07 17:46:25 +0100

    ajout énum Equipement

--- src/personnages/Equipement.java
+++ src/personnages/Equipement.java
@@ -1,5 +1,15 @@
 package personnages;
 
 public enum Equipement {
-	CASQUE, BOUCLIER;
+	CASQUE("casque"), BOUCLIER("bouclier");
+	
+	private String nom;
+	
+	private Equipement(String nom) {
+		this.nom=nom;
+	}
+	
+	public String toString() {
+		return nom;
+	}
 }
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,6 +3,9 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipement= new Equipement[2];
+	private int nbEquipement=0;
+	
 	public Romain(String nom, int force) {
 			this.nom = nom;
 			this.force = force;
@@ -29,8 +32,20 @@ public class Romain {
 				parler("J'abandonne...");
 }
 	}
+	
+	public String sEquiper(Equipement equipementajouté) {
+		if (equipement[1]== Equipement.CASQUE) {
+			return "Le romain " + nom + "est déja bien protégé";
+		}
+	}
 	public static void main(String[] args) {
 		final Romain minus =new Romain("Minus",6);
 		System.out.println(Equipement.CASQUE);
+		System.out.println(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		
 }
 }
